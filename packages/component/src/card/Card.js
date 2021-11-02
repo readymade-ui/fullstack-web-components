44b35fc0c9362a6f25dc70c99d54556c2b4d2f0a
Steve Belovarich <steveblue@gmail.com>
2021-11-01 18:51:05 -0700

    chapter 1 styling CardComponent solution

--- packages/component/src/card/Card.js
+++ packages/component/src/card/Card.js
@@ -4,6 +4,31 @@ export class CardComponent extends HTMLElement {
     const shadowRoot = this.attachShadow({ mode: 'open' });
     const template = document.createElement('template');
     template.innerHTML = `
+    <style>
+      :host {
+        display: block;
+        background: var(--color-white);
+        border-radius: var(--radius-md);
+        box-shadow: var(--shadow);
+        overflow: hidden;
+        max-width: 320px;
+      }
+      ::slotted(*) {
+        padding-left: var(--padding-lg);
+        padding-right: var(--padding-lg);
+      }
+      ::slotted(a:link), ::slotted(a:visited) {
+        display: block;
+      }
+      ::slotted(:last-child) {
+        padding-bottom: var(--margin-lg);
+      }
+      ::slotted(img) {
+        width: 100%;
+        padding-left: 0px;
+        padding-right: 0px;
+      }      
+    </style>
     <header>
       <slot name="header"></slot>
     </header>
