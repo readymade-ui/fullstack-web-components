3752a85b83173e8b8fb71d393ce37c860659d317
Stephen Belovarich <steveblue@gmail.com>
2022-02-13 12:21:58 -0800

    fix: webpack 5 fixes build issues

--- packages/client/src/polyfill.ts
+++ packages/client/src/polyfill.ts
@@ -1,3 +1,20 @@
 export * from 'broadcastchannel-polyfill';
 export * from '@ungap/custom-elements';
 export * from 'element-internals-polyfill';
+
+// Declarative Shadow DOM Polyfill
+// (function attachShadowRoots(root: Document | ShadowRoot) {
+//   root.querySelectorAll('template[shadowroot]').forEach((template) => {
+//     const mode = template.getAttribute('shadowroot');
+//     const shadowRoot: ShadowRoot = (
+//       template.parentNode as HTMLElement
+//     ).attachShadow({
+//       mode: 'open',
+//     });
+//     shadowRoot.appendChild(
+//       ((<unknown>template) as HTMLTemplateElement).content
+//     );
+//     template.remove();
+//     attachShadowRoots(shadowRoot);
+//   });
+// })(document);
