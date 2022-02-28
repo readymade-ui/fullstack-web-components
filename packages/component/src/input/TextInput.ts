fcf91b4e85d2a71103c31070872421c831fa17a6
Stephen Belovarich <steveblue@gmail.com>
2022-02-27 17:33:42 -0800

    fix: form-associeted type definitions

--- packages/component/src/input/TextInput.ts
+++ packages/component/src/input/TextInput.ts
@@ -1,5 +1,5 @@
 import {
-  ElementInternals,
+  IElementInternals,
   ValidityStateFlags,
 } from 'types/lib.elementInternals';
 import { Component, attachShadow, Listen, html, css } from '@in/common';
@@ -67,8 +67,8 @@ import { validate, Validator } from './validator';
 })
 export class TextInputComponent extends HTMLElement {
   static formAssociated = true;
-  private attachInternals: () => ElementInternals;
-  private internals: ElementInternals;
+  private internals: IElementInternals;
+  public attachInternals: () => IElementInternals;
   public $validator: Validator;
   private $attr = {};
   constructor() {
