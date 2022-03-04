92ee58d935173c84d55e85841ebf92e8b92fab20
Stephen Belovarich <steveblue@gmail.com>
2022-03-04 12:51:00 -0800

    fix: add services to header and footer

--- packages/client/src/service/cookies.ts
+++ packages/client/src/service/cookies.ts
@@ -29,6 +29,26 @@ export class CookieService {
       })
       .then((res: InternalCookiePermission) => this.processPermission(res));
   }
+  givePermission(cookies: CookiePermission): Promise<CookiePermission> {
+    const allow = cookies.permission === COOKIES.ACCEPT ? true : false;
+    return fetch('/api/cookies', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ permission: allow }),
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          return res.json();
+        } else {
+          return {
+            permission: false,
+          };
+        }
+      })
+      .then((res: InternalCookiePermission) => this.processPermission(res));
+  }
   processPermission(model: InternalCookiePermission): CookiePermission {
     return {
       permission: model.permission ? COOKIES.ACCEPT : COOKIES.DECLINE,
