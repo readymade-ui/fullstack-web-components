021e8907cdf060f104082e403b056285606eed99
Stephen Belovarich <steveblue@gmail.com>
2022-06-16 22:03:53 -0700

    fix: storybook config should not compile to es5

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,12 @@
 module.exports = {
-  "stories": [
-    "./../packages/component/**/*.stories.@(js|jsx|ts|tsx)",
+  stories: ['./../packages/component/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/web-components",
-  "core": {
-    "builder": "webpack5"
-  }
-}
+  framework: '@storybook/web-components',
+  core: {
+    builder: 'webpack5',
+  },
+};
