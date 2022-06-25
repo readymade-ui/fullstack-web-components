184c3478f89faf6eeb250c5ac9b59ea72a936c6c
jurerick <jurerick.porras@gmail.com>
2022-06-25 23:28:34 +0800

    feat: card scaffold

---
+++ packages/component/src/card/Card.stories.js
@@ -0,0 +1,48 @@
+import { CardComponent } from "./Card";
+
+export default {
+    title: "Components/Card",
+    argTypes: {
+        image: {
+            control: {
+                type: 'text'
+            }
+        },
+        headline: {
+            control: {
+                type: 'text'
+            }
+        },
+        content: {
+            control: {
+                type: 'text'
+            }
+        }
+    }
+}
+
+const PrimaryTemplate = ({
+    image, headline, content, link
+}) => `
+    <in-card>
+        <img 
+            slot='header' 
+            src='${image}'
+        />
+
+        <h4 slot='header'>${headline}</h4>
+        
+        <p slot='content'>${content}</p>
+        
+        <a href='#' slot='footer'>${link}</a>
+    </in-card>
+`;
+
+export const ImageCard = PrimaryTemplate.bind({});
+
+ImageCard.args = {
+    image: 'https://www.denofgeek.com/wp-content/uploads/2021/04/jujutsu-kaisen-season-2-movie-prequel-details.jpeg?fit=1920%2C1080',
+    headline: 'Food',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing eli...',
+    link: 'Read'
+};
