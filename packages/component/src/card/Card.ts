acc51a72f19557c4e2c1292fcf8d0a07486fe207
jurerick <jurerick.porras@gmail.com>
2022-06-26 11:21:12 +0800

    feat: update shadow DOM style

--- packages/component/src/card/Card.ts
+++ packages/component/src/card/Card.ts
@@ -9,6 +9,34 @@ export class CardComponent extends HTMLElement {
 
         const template = document.createElement('template');
         template.innerHTML = `
+            <style>
+                :host {
+                    display: block;
+                    background: var(--color-white);
+                    border-radius: var(--radius-md);
+                    box-shadow: var(--shadow);
+                    width: 320px;
+                    overflow: hidden;
+                }
+                ::slotted(*) {
+                    padding-left: var(--padding-lg);
+                    padding-right: var(--padding-lg);
+                }
+                ::slotted(a:link), ::slotted(a:visited){
+                    display: block;
+                }
+                ::slotted(:last-child) {
+                    padding-bottom: var(--margin-lg);
+                }
+                ::slotted(img) {
+                    width: 100%;
+                    padding-left: 0;
+                    padding-right: 0;
+                }
+                ::slotted(h4){
+                    font-size: var(--font-headline-sm) !important;
+                }
+            </style>
             <header>
                 <slot name='header'></slot>
             </header>
