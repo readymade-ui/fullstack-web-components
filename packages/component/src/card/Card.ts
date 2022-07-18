7e0a8026db63235a828e2ca392327db19a8fba45
Alon Golan <alongolan@Alons-MacBook-Pro-2.local>
2022-07-18 19:38:56 +0300

    Style Card

--- packages/component/src/card/Card.ts
+++ packages/component/src/card/Card.ts
@@ -1,10 +1,35 @@
 export class CardComponent extends HTMLElement {
-  constructor(){
+  constructor() {
     super();
-    const shadowRoot = this.attachShadow({mode: 'open'});
+    const shadowRoot = this.attachShadow({ mode: 'open' });
     const template = document.createElement('template');
     template.innerHTML =
-      `<header>
+      `
+      <style>
+        :host {
+          display: block;
+          background: var(--color-white);
+          border-radius: var(--radius-md);
+          box-shadow: var(--shadow);
+          overflow: hidden;
+          max-width: 320px;
+        }
+       ::slotted(*) {
+        padding: 0 var(--padding-lg);
+       }
+       ::slotted(a:link),
+       ::slotted(a:visited) {
+        display: block;
+       }
+       ::slotted(:last-child) {
+       margin-bottom: var(--margin-lg);
+       }
+       ::slotted(img){
+        width: 100%;
+        padding: 0;
+       }
+      </style>
+      <header>
             <slot name='header'></slot>
       </header>
       <section>
